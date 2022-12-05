d11fbba52a31733572ea069cf890f1c9653945ea
gabrymattioli <gabrymattioli@gmail.com>
2022-12-05 11:29:14 +0100

    RepNet, main: refactoring

--- RepNet-PyTorch/main.py
+++ RepNet-PyTorch/main.py
@@ -1,5 +1,9 @@
+import os
+import time
+
 import torch
 import random
+import numpy as np
 
 from torch.utils.data import DataLoader, ConcatDataset, RandomSampler
 
@@ -19,27 +23,12 @@ frame_per_vid = 64
 multiple = False
 
 if __name__ == '__main__':
-    '''
-    deadlift_dataset = getCombinedDataset('deadlift_videos/repnet.csv',
-                                       'deadlift_videos',
-                                       'Good')
-    
-    dataset_size = len(deadlift_dataset)
-    val_size = int(np.floor(0.2 * dataset_size))
-    train_size = dataset_size - val_size
-    train_set, val_set = torch.utils.data.random_split(deadlift_dataset, [train_size, val_size], generator=torch.Generator().manual_seed(42))
-    
-    
-    
-    
+    '''        
     # testDatasetC = getCombinedDataset('countix/countix_test.csv',
     #                                    'testvids',
     #                                    'test')
     
-    # testDatasetC = getCombinedDataset('deadlift_downscaled_360p/repnet.csv',
-    #                                    'deadlift_downscaled_360p',
-    #                                    'Good')
-    testDatasetS = SyntheticDataset('synthvids', 'train*', 'mp4', 2000)
+
     
     testList = [testDatasetC] #, testDatasetS]
     random.shuffle(testList)
@@ -50,38 +39,66 @@ if __name__ == '__main__':
     # trainDatasetC = getCombinedDataset('countix/countix_train.csv',
     #                                    'trainvids',
     #                                    'train')
-    trainDatasetC = getCombinedDataset('deadlift_videos/repnet.csv',
-                                       'deadlift_videos',
-                                       'Good')
-    #trainDatasetS1 = SyntheticDataset('/home/saurabh/Downloads/HP72','HP72', 'mp4', 500,
-    #                                   frame_per_vid=frame_per_vid)
+
     #trainDatasetS2 = SyntheticDataset('/home/saurabh/Downloads', '1917', 'mkv', 500,
     #                                   frame_per_vid=frame_per_vid)
     
     '''
 
-    trainDatasetS3 = SyntheticDataset('synthvids', '*', 'mp4', 3000)
     #trainDatasetS4 = SyntheticDataset('/home/saurabh/Downloads', 'HP6', 'mkv', 500,
     #                                   frame_per_vid=frame_per_vid)
-    trainDatasetB = BlenderDataset('blendervids', 'videos', 'annotations', frame_per_vid)
+    # trainDatasetB = BlenderDataset('blendervids', 'videos', 'annotations', frame_per_vid)
 
-    trainList = [trainDatasetS3] #, trainDatasetB]
-    random.shuffle(trainList)
-    trainDataset = ConcatDataset(trainList)
+    # testDatasetC = getCombinedDataset('deadlift_downscaled_360p/repnet.csv',
+    #                                   'deadlift_downscaled_360p',
+    #                                   'Good')
+    # testDatasetS = SyntheticDataset('synthvids', 'train*', 'mp4', 2000)
+    #
+    # trainDatasetS3 = SyntheticDataset('synthvids', '*', 'mp4', 3000)
+    # trainDatasetC = getCombinedDataset('deadlift_videos/repnet.csv',
+    #                                    'deadlift_videos',
+    #                                    'Good')
 
-    print("done")
+    print(f'CARTELLA: {os.listdir()}\n\n')
+    time.sleep(1000)
 
-    """Testing the training loop with sample datasets"""
+    dataset = getCombinedDataset(
+        'deadlift_downscaled_540p/repnet.csv',
+        'deadlift_downscaled_540p_reps',
+        'Good'
+    )
+    synthetic_dataset = SyntheticDataset('deadlift_downscaled_540p_reps/', '*', 'mp4', 3000)
 
-    sampleDatasetA = torch.utils.data.Subset(trainDataset, range(0, len(trainDataset)))
+    dataset_size = len(dataset)
+    val_size = int(np.floor(0.2 * dataset_size))
+    train_size = dataset_size - val_size
+    comb_train_set, comb_val_set = torch.utils.data.random_split(
+        dataset, [train_size, val_size], generator=torch.Generator().manual_seed(42)
+    )
+
+    synthetic_dataset_size = len(synthetic_dataset)
+    synthetic_val_size = int(np.floor(0.2 * synthetic_dataset_size))
+    synthetic_train_size = synthetic_dataset_size - synthetic_val_size
+    synthetic_train_set, synthetic_val_set = torch.utils.data.random_split(
+        synthetic_dataset, [synthetic_train_size, synthetic_val_size],
+        generator=torch.Generator().manual_seed(42)
+    )
+
+    train_dataset_list = [comb_train_set, synthetic_train_set]
+    random.shuffle(train_dataset_list)
+    train_set = ConcatDataset(train_dataset_list)
+
+    val_dataset_list = [comb_val_set, synthetic_val_set]
+    random.shuffle(val_dataset_list)
+    val_set = ConcatDataset(val_dataset_list)
+
+    # sampleDatasetA = torch.utils.data.Subset(trainDataset, range(0, len(trainDataset)))
     # sampleDatasetB = torch.utils.data.Subset(testDataset, range(0,  len(testDataset)))
-
-    print(len(sampleDatasetA))
 
     trLoss, valLoss = training_loop(
         50,
-        sampleDatasetA,
-        sampleDatasetA,
+        train_set,
+        val_set,
         1,
         lr=6e-5,
         ckpt_name='x3dbb',
