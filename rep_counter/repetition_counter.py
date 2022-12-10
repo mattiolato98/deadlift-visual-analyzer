fbeaa6521959ded5575c563e76f4b54c730bf2e7
gabrymattioli <gabrymattioli@gmail.com>
2022-12-10 01:21:18 +0100

    rep_counter: added frame_number keyword argument and refactored self._pose_entered assignment.
    This allows to check if the barbell is moving before to enter the pose 'up', avoiding potential false positives.

--- rep_counter/repetition_counter.py
+++ rep_counter/repetition_counter.py
@@ -21,13 +21,17 @@ class RepetitionCounter(object):
     def n_repeats(self):
         return self._n_repeats
 
-    def __call__(self, pose_classification):
+    def __call__(self, pose_classification, frame_number=None):
         """Counts number of repetitions happend until given frame.
 
         We use two thresholds. First you need to go above the higher one to enter
         the pose, and then you need to go below the lower one to exit it. Difference
         between the thresholds makes it stable to prediction jittering (which will
         cause wrong counts in case of having only one threshold).
+
+        The frame_number allows to check if the barbell is moving in the current frame,
+        avoiding to enter the 'up' pose when the athlete is only setting up, counting
+        consequentially more reps.
 
         Args:
           pose_classification: Pose classification dictionary on current frame.
@@ -40,6 +44,17 @@ class RepetitionCounter(object):
         Returns:
           Integer counter of repetitions.
         """
+        def is_pose_entered():
+            # If motion_frames and frame_number are provided, check both that
+            # pose_confidence is greater than _enter_threshold and that the
+            # barbell is moving in the current frame. Otherwise, check only
+            # for the threshold.
+            if self._motion_frames is not None and frame_number is not None:
+                return (pose_confidence > self._enter_threshold and
+                        frame_number in self._motion_frames)
+            else:
+                return pose_confidence > self._enter_threshold
+
         # Get pose confidence.
         pose_confidence = 0.0
         if self._class_name in pose_classification:
@@ -48,7 +63,7 @@ class RepetitionCounter(object):
         # On the very first frame or if we were out of the pose, just check if we
         # entered it on this frame and update the state.
         if not self._pose_entered:
-            self._pose_entered = pose_confidence > self._enter_threshold
+            self._pose_entered = is_pose_entered()
             return self._n_repeats
 
         # If we were in the pose and are exiting it, then increase the counter and
