f7f8528aad9e3305102709ab26ba6cfa175a660a
gabrymattioli <gabrymattioli@gmail.com>
2023-02-06 19:20:20 +0100

    motion_utils.py: Added property motion_frames. Always return extended motion frames, filling the gap before the beginning of each repetition

--- motion_detection/motion_utils.py
+++ motion_detection/motion_utils.py
@@ -1,3 +1,5 @@
+import copy
+
 from statistics import mean
 
 
@@ -9,11 +11,29 @@ class MotionDetector:
         self.first_motion = False
         self.threshold = threshold
         self.frame_number = frame_number
-        self.motion_frames = []
+        self._motion_frames = []
 
     @property
     def mean_y(self):
         return mean(self.positions) if len(self.positions) > 0 else 0
+
+    @property
+    def motion_frames(self):
+        """Before returning motion frames list, extends it adding 2 * fps frame values before each gap.
+        Since motion frames are ideally recorded when the barbell is moving, then it is necessary to
+        go back of a small amount of frames to catch the start of a repetition.
+        """
+        sec = int(self.fps) * 2
+
+        extended_motion_frames = copy.deepcopy(self._motion_frames)
+
+        for idx, value in enumerate(extended_motion_frames):
+            if value - extended_motion_frames[idx - 1] > sec:
+                extended_motion_frames.extend([i for i in range(value - sec, value)])
+
+        extended_motion_frames.sort()
+
+        return extended_motion_frames
 
     def build_ground_position(self, y):
         """ Compute the average y value, when the barbell is on the ground.
@@ -41,4 +61,4 @@ class MotionDetector:
             self.first_motion = self.is_motion_frame(y)
 
         if self.is_motion_frame(y):
-            self.motion_frames.append(frame)
+            self._motion_frames.append(frame)
