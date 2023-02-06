275f295dc000875f31bff0b9d4961d70c6a73b75
gabrymattioli <gabrymattioli@gmail.com>
2023-02-06 17:57:44 +0100

    motion_utils.py: Created class for motion detection operations

---
+++ motion_detection/motion_utils.py
@@ -0,0 +1,43 @@
+from statistics import mean
+
+
+class MotionDetector:
+    def __init__(self, threshold=10, frame_number=0):
+        self.barbell_motion = []
+        self.positions = []
+        self.first_motion = False
+        self.threshold = threshold
+        self.frame_number = frame_number
+        self.motion_frames = []
+
+    @property
+    def mean_y(self):
+        return mean(self.positions) if len(self.positions) > 0 else 0
+
+    def build_ground_position(self, y):
+        """ Compute the average y value, when the barbell is on the ground.
+        Args:
+            y: current position of the bounding box along y-axis
+        """
+        self.positions.append(y)
+
+    def is_motion_frame(self, y):
+        """ Detect if the current frame is a motion frame (a frame in which the barbell is not on the ground).
+        Args:
+            y: current position of the bounding box along y-axis
+
+        Returns:
+            True if bb movement along y-axis exceeds threshold
+        """
+        if abs(self.mean_y - y) > self.threshold:
+            return True
+
+        return False
+
+    def detect_motion(self, y, frame):
+        if not self.first_motion:
+            self.build_ground_position(y)
+            self.first_motion = self.is_motion_frame(y)
+
+        if self.is_motion_frame(y):
+            self.motion_frames.append(frame)
