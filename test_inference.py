b8c45ee83847f0ae4c02ae8e9923f2d44dcb4980
Filippo Rinaldi <fillor.1998@gmail.com>
2023-02-13 12:37:24 +0100

    test_inference: modified for debug purposes

--- test_inference.py
+++ test_inference.py
@@ -15,7 +15,11 @@ if __name__ == '__main__':
 
             predictions = inference(f'{PATH}{filename}', yolo_detection=True)
             # results[filename].append(predictions)
+            print(predictions)
             results[filename] = predictions
+            for id, pred in enumerate(results[filename]):
+                # print(pred)
+                print(f"Repetition number {id + 1} of {len(predictions)} : judged ", "Good" if pred else "Bad")
 
     for video in results:
         show_results(video, results[video])
