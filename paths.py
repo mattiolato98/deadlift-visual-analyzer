8be3874e1cc563917bd0b796e701b0b605920d25
gabrymattioli <gabrymattioli@gmail.com>
2023-02-18 15:39:02 +0100

    Created paths.py

---
+++ paths.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+
+WEIGHTS_PATH = 'custom_weights/yolo_best_weights.pt'
+RESULTS_PATH = Path('results')
+CLASS_NAME = 'deadlift_up'
+POSE_SAMPLES_PATH = 'deadlift_poses'
+GROUND_TRUTH_CSV = 'test/reps.csv'
